cb72e7faaf3864cafb50e4db3f77db51e5598a3a
unknown <yrahul9452@gmail.com>
2023-01-20 12:26:18 +0530

    newstate updated

--- reducer/redcuer2/src/App.js
+++ reducer/redcuer2/src/App.js
@@ -8,6 +8,12 @@ function App() {
       <h1>Reducer App</h1>
       <h1>Reducer App</h1>
       <h1>Reducer App</h1>
+      <h1>Reducer App</h1>
+      <h1>Reducer App</h1>
+      <h1>Reducer App</h1>
+      <h1>Reducer App</h1>
+      <h1>Reducer App</h1>
+      <h1>Reducer APP2</h1>
     </div>
   );
 }
